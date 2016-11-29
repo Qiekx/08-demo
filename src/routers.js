f94cd7afbf5a721dc6d7d3e3f1bd1d9481cf4fa7
Qiekx <1014687905@qq.com>
2016-11-29 11:53:07 +0800

    i

---
+++ src/routers.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
+import App from './App';
+import Home from './Home/index';
+import Blog from './Blog/index';
+import Work from './Work/index';
+import About from './About/index';
+
+class Routers extends React.Component {
+  render () {
+    return(
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home} />
+          <Route path="blog" component={Blog} />
+          <Route path="work" component={Work} />
+          <Route path="about" component={About} />
+
+        </Route>
+      </Router>
+    )
+  }
+}
+
+export default Routers;
