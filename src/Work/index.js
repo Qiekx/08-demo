a2c9d946bdf0c8758a41d82a595572195bc038af
Qiekx <1014687905@qq.com>
2016-12-01 15:05:40 +0800

    work

--- src/Work/index.js
+++ src/Work/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import echarts from 'echarts'
 
 class Work extends React.Component {
+  componentDidMount(){
+    var myChart = echarts.init(document.getElementById('main'));
+    myChart.setOption({
+      title: {
+         text: '我的技能展示',
+         textStyle:{
+           color:'teal'
+         },
+         left:'center'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["html","css","js","react","node","mangodb"]
+      },
+      yAxis: {},
+      series: [{
+        name: '熟练度',
+        type: 'bar',
+        data: [90, 95, 85, 80, 70, 60],
+        color:['blue']
+
+      }]
+    });
+  }
   render () {
     return(
-      <div>
+      <div id='main' style={{width:'100%',heigt:'auto',maxWidth:'600px',margin:'0 auto'}}>
         Work
       </div>
     )
