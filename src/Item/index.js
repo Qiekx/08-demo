d5d4f83e0a88223b26831be9aaa05e01bd042755
Qiekx <1014687905@qq.com>
2016-12-03 21:42:29 +0800

    i

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,24 +1,25 @@
 import React, { PropTypes } from 'react'
 import marked from 'marked';
 import axios from 'axios';
+import Loading from '../component/Loading';
 
 class Item extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      data:''
+    }
+  }
   componentDidMount(){
     let address = this.props.params.title;
     axios.get(`https://raw.githubusercontent.com/Qiekx/08-demo/master/data/${address}.md`)
-      .then( res => console.log(res))
+      .then( res => this.setState({data:res.data}))
+      .catch(err => alert(err))
   }
   render () {
-    let content = this.props.params.title == 1 ? '这是第一个页面' :
-    this.props.params.title == 2 ? '这是第二个页面' :
-    this.props.params.title == 3 ? '这是第三个页面' :
-    this.props.params.title == 4 ? '这是第四个页面' :
-    this.props.params.title == 5 ? '这是第五个页面' :
-    this.props.params.title == 6 ? '这是第六个页面' : '这是第n个页面'
     return(
-      <div>
-        {content}
-
+      <div className="item-wrap">
+        {this.state.data.length == 0 ? <Loading /> : <div dangerouslySetInnerHTML={{__html:marked(this.state.data)}}/>}
       </div>
     )
   }
