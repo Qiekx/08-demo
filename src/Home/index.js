e2367829b2810e503c91f06234c0b765d995cec1
Qiekx <1014687905@qq.com>
2017-02-20 11:34:26 +0800

    i

--- src/Home/index.js
+++ src/Home/index.js
@@ -8,8 +8,8 @@ class Home extends React.Component {
           <div className="home-item">
             <h3>HI, I'M QIEKX</h3>
             <p>WEB DEVELOPER</p>
-            <RaisedButton label="HIRE ME" primary={true}  href="https://Qiekx.github.io"  />
-          
+            <RaisedButton label="HIRE ME" secondary={true}  href="https://Qiekx.github.io"  />
+
           </div>
         </div>
       </div>
